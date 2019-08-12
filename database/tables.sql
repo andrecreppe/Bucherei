b8338b8528b096812a722d834c7abdc7413efc6d
andrecreppe <andre.creppe@uol.com.br>
2019-08-11 22:32:54 -0300

    - SQL minor changes 11-08-19

--- database/tables.sql
+++ database/tables.sql
@@ -1,11 +1,35 @@
+/*----------------------------------------------------*/
+
+DROP TABLE IF EXISTS configuracoes;
+CREATE TABLE configuracoes
+(
+    id              SERIAL  NOT NULL PRIMARY KEY,
+    dias_devolucao  INTEGER NOT NULL
+);
+
+DROP TABLE IF EXISTS usuarios;
+CREATE TABLE usuarios
+(
+    id          SERIAL          NOT NULL PRIMARY KEY,
+    nome        VARCHAR(40)     NOT NULL,
+    sobrenome   VARCHAR(80)     NOT NULL,
+    cpf         VARCHAR(11)     NOT NULL,
+    email       VARCHAR(100)    NOT NULL,
+    senha       VARCHAR(70)     NOT NULL
+);
+
+/*----------------------------------------------------*/
+
+DROP TABLE IF EXISTS secoes;
 CREATE TABLE secoes
 (
     id          SERIAL      NOT NULL PRIMARY KEY,
     nome        VARCHAR(60) NOT NULL,
-    descricao   VARCHAR(50)
+    descricao   TEXT        DEFAULT NULL,
     ativa       BOOLEAN NOT NULL
 );
 
+DROP TABLE IF EXISTS livros;
 CREATE TABLE livros
 (
     id          SERIAL      NOT NULL PRIMARY KEY,
@@ -17,25 +41,23 @@ CREATE TABLE livros
     id_secao    INTEGER     NOT NULL REFERENCES secoes
 );
 
-/*---------------------------*/
+/*----------------------------------------------------*/
 
-CREATE TABLE usuarios
-(
-    id          SERIAL NOT NULL PRIMARY KEY,
-    nome        VARCHAR(40) NOT NULL,
-    sobrenome   VARCHAR(80) NOT NULL
-    cpf         VARCHAR(11) NOT NULL,
-    email       VARCHAR(100) NOT NULL,
-    senha       VARCHAR(70) NOT NULL
-);
-
-/*---------------------------*/
-
+DROP TABLE IF EXISTS alugados;
 CREATE TABLE alugados
 (
     id              SERIAL  NOT NULL PRIMARY KEY,
     id_usuario      INTEGER NOT NULL REFERENCES usuarios,
     id_livro        INTEGER NOT NULL REFERENCES livros,
-    data_aluga      DATE    NOT NULL,
-    data_devolucao  DATE
+    data_retirada   DATE    NOT NULL,
+    data_devolucao  DATE    DEFAULT NULL
+);
+
+DROP TABLE IF EXISTS multas;
+CREATE TABLE multas
+(
+    id          SERIAL  NOT NULL PRIMARY KEY,
+    id_usuario  INTEGER NOT NULL REFERENCES usuarios,
+    id_livro    INTEGER NOT NULL REFERENCES livros,
+    data        DATE    NOT NULL
 );
